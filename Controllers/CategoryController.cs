fd18047aaf1bb0d00ad721b78f310158bf6c1cb2
Ritu Ban <ritu.160410107008@gmail.com>
2025-07-31 23:03:28 +0530

    Added CategoryController

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using InventoryAPI.Data;
+using InventoryAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/category
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> GetCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+        // POST: api/category
+        [HttpPost]
+        public ActionResult<Category> AddCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetCategories), new { id = category.Id }, category);
+        }
+    }
+}
